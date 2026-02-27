1b06555cc5ee86ac3f3f36e141b58b486ca9bb06
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-02-27 21:46:42 +0530

    Add palindrome number checker in palindrome_number.c

---
+++ Day10_Nested_Loops_and_Palindrome/palindrome_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int val,n;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    
+    val=n;
+    int rem,rev=0;
+    while(n!=0){
+        rem=n%10;
+        rev=rev*10+rem;
+        n/=10;
+    }
+    if(rev==val){
+        printf("The Entered number is an palindrome.");
+    }
+    else{
+        printf("The Entered number is not an palindrome.");
+    }
+}
