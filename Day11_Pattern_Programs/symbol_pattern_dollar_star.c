b5b0f84646243f368068ba4b7144c7d169019bf6
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-05 22:53:20 +0530

    Add symbol pattern program with dollar and star

---
+++ Day11_Pattern_Programs/symbol_pattern_dollar_star.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int n,m;
+    //printf("Enter n : ");
+    scanf("%d",&n);
+    //printf("Enter m : ");
+    scanf("%d",&m);
+    
+    int side=(n-m)/2;
+    for(int i=0;i<m;i++){
+        printf("$");
+    }
+    for(int j=0;j<side;j++){
+            printf("*");
+    }
+    for(int i=0;i<m;i++){
+        printf("$");
+    }
+    
+}
