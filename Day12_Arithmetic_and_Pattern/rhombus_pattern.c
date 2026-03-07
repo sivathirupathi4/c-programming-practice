208506048353c719417a3524c2ae33cdfc76938d
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-07 09:50:35 +0530

    Implement rhombus pattern printing in C
    This program prints a rhombus pattern based on the input value of n.

--- Day12_Arithmetic_and_Pattern/rhombus_pattern.c
+++ Day12_Arithmetic_and_Pattern/rhombus_pattern.c
@@ -1,1 +1,27 @@
-h
+#include <stdio.h>
+int main()
+{
+    int n;
+   // printf("Enter n value: ");
+    scanf("%d",&n);
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*i-1;k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=2*(n-i)-1;k++){
+          printf("*");   
+        }
+        printf("\n");
+    }
+    return 0;
+}
