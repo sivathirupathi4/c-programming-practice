1230092e9e1efc66395250b433343f492bcdcf7e
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-09 22:40:40 +0530

    Add program to compute AP, GP, and HP series
    This program calculates and displays the Arithmetic Progression (AP), Geometric Progression (GP), and Harmonic Progression (HP) based on user input for the first term, common difference/ratio, and number of terms.

---
+++ Day13_Number_Programs/ap_gp_hp_series.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+    int a,r,n;
+    printf("Enter the First Number 'A': ");
+    scanf("%d",&a);
+    printf("Enter the Common Difference / Ratio 'R': ");
+    scanf("%d",&r);
+    printf("Enter the Number of terms 'N': ");
+    scanf("%d",&n);
+    
+    if(n>0){
+    int AP=a;
+    printf("AP = ");
+    for(int i=1;i<=n;i++){
+        printf("%d, ",AP);
+        AP=AP+r;
+    }
+    printf("\n");
+    
+    int GP=a;
+    printf("GP = ");
+    for(int j=1;j<=n;j++){
+        printf("%d, ",GP);
+        GP=GP*r;
+    }
+    printf("\n");
+    
+    float HP=1;
+    AP=a;
+    printf("HP =");
+    for(int i=1;i<=n;i++){
+        
+        printf(" %f, ",((float)HP)/AP);
+        AP=AP+r;
+    }
+    }
+    else{
+        printf("Invalid input");
+    }
+    
+}
