855f4e014bf74bef52a87a220f091e175d47a0cb
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-10 10:47:26 +0530

    Add pyramid number pattern program in C

---
+++ Day14_Patterns/pyramid_number_pattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int n;
+    //printf("Enter the number: ");
+    scanf("%d",&n);
+    
+    for(int i=n;i>=1;i--){
+        for(int j=i;j<=n;j++){
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    for(int k=2;k<=n;k++){
+        for(int l=k;l<=n;l++){
+            printf("%d ",l);
+        }
+        printf("\n");
+    }
+}
