c484a1cc5e29f77b492edb26f8dcabd7f5fe0dac
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-10 10:46:32 +0530

    Add X pattern number printing program
    Implement a program to print an X pattern using numbers based on user input.

---
+++ Day14_Patterns/x_pattern_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int lines;
+    scanf("%d",&lines);
+    
+    for(int i=1;i<=lines;i++){
+        for(int j=1;j<=lines;j++){
+            if((i==j) || (i+j)==(lines+1)){
+                printf("%d",j);
+                
+            }else{
+                printf(" ");
+            }
+            
+            }
+            printf("\n");
+    }
+}
