5f044e23e6a93fb3ab42020301ad0bb9be3dbafe
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-11 12:26:06 +0530

    Refactor rhombus pattern printing logic

--- Day12_Arithmetic_and_Pattern/rhombus_pattern.c
+++ Day12_Arithmetic_and_Pattern/rhombus_pattern.c
@@ -5,20 +5,26 @@ int main()
    // printf("Enter n value: ");
     scanf("%d",&n);
     
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+        {
             printf(" ");
         }
-        for(int k=1;k<=2*i-1;k++){
+        for(int k=1;k<=2*i-1;k++)
+        {
             printf("*");
         }
         printf("\n");
     }
-    for(int i=1;i<=n-1;i++){
-        for(int j=1;j<=i;j++){
+    for(int i=1;i<=n-1;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
             printf(" ");
         }
-        for(int k=1;k<=2*(n-i)-1;k++){
+        for(int k=1;k<=2*(n-i)-1;k++)
+        {
           printf("*");   
         }
         printf("\n");
