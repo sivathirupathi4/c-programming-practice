c45a4687796862d98cfbd5f88422e586831b3c2e
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-14 14:28:20 +0530

    Refactor character increment pattern code
    Refactor nested loops for character increment pattern.

--- Day10_Nested_Loops_and_Palindrome/character_increment_pattern.c
+++ Day10_Nested_Loops_and_Palindrome/character_increment_pattern.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int n;
     char ch='A';
     
     //printf("Enter max lines: ");
     scanf("%d",&n);
     
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
             printf("%c ",ch++);
             
         }
