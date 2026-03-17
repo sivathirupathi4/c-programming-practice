809c480a1360828343aaca34caa3d9b54fed76e4
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-17 23:03:39 +0530

    Refactor pyramid number pattern logic

--- Day14_Patterns/pyramid_number_pattern.c
+++ Day14_Patterns/pyramid_number_pattern.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int n;
     //printf("Enter the number: ");
     scanf("%d",&n);
     
-    for(int i=n;i>=1;i--){
-        for(int j=i;j<=n;j++){
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=i;j<=n;j++)
+        {
             printf("%d ",j);
         }
         printf("\n");
     }
-    for(int k=2;k<=n;k++){
-        for(int l=k;l<=n;l++){
+    for(int k=2;k<=n;k++)
+    {
+        for(int l=k;l<=n;l++)
+        {
             printf("%d ",l);
         }
         printf("\n");
