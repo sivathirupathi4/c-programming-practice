41dff4d808aad729bc7642af4110d3d23ce73812
sivathirupathi4 <sivathirupathi4@gmail.com>
2026-03-17 23:04:08 +0530

    Refactor nested loops and formatting in x_pattern_numbers
    Refactor x_pattern_numbers.c for improved readability.

--- Day14_Patterns/x_pattern_numbers.c
+++ Day14_Patterns/x_pattern_numbers.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int lines;
     scanf("%d",&lines);
     
-    for(int i=1;i<=lines;i++){
-        for(int j=1;j<=lines;j++){
-            if((i==j) || (i+j)==(lines+1)){
+    for(int i=1;i<=lines;i++)
+    {
+        for(int j=1;j<=lines;j++)
+        {
+            if((i==j) || (i+j)==(lines+1))
+            {
                 printf("%d",j);
                 
-            }else{
+            }
+            else
+            {
                 printf(" ");
             }
             
